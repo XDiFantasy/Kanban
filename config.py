1dab68a2c5780579166871dcf6023c9eecd919c6
zwkatgithub <zhangwenkai2013@163.com>
2018-04-08 11:28:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,49 @@
+import os
+from flask_appbuilder.security.manager import AUTH_DB
 
 sms_key = "24be6ffdbdc18"
 
-BASE = "mysql+pymysql://user:password@192.168.0.{0}:3306/db"
-READ_DATABASE = BASE.format(250)
-WRITE_DATABASE = BASE.format(251)
+_user = os.getenv('mysql_user')
+_passwd = os.getenv('mysql_passwd')
+_host = os.getenv('mysql_host')
+_db = os.getenv('mysql_db')
+print('use env variables')
+BASE = "mysql+pymysql://"+_user+":"+_passwd+"@"+_host+":3306/"+_db
+READ_DATABASE = BASE
+WRITE_DATABASE = BASE
+
 
 class Config:
     SECRET_KEY = "miyao"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@localhost:3306/dbname" #WRITE_DATABASE
-    SQLALCHEMY_TRACK_MODIFICATIONS =True
-    
+    SQLALCHEMY_DATABASE_URI = WRITE_DATABASE
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    CSRF_ENABLED = True
+    APP_NAME = "I.B.G"  
+    AUTH_TYPE = AUTH_DB
+    BABEL_DEFAULT_LOCALE = 'zh'
+    BABEL_DEFAULT_FOLDER = 'translations'
+    LANGUAGES = {
+    'en': {'flag':'gb', 'name':'English'},
+    'pt': {'flag':'pt', 'name':'Portuguese'},
+    'pt_BR': {'flag':'br', 'name': 'Pt Brazil'},
+    'es': {'flag':'es', 'name':'Spanish'},
+    'de': {'flag':'de', 'name':'German'},
+    'zh': {'flag':'cn', 'name':'Chinese'},
+    'ru': {'flag':'ru', 'name':'Russian'},
+    'pl': {'flag':'pl', 'name':'Polish'}}
+    APP_THEME = "bootstrap-theme.css"
+
 
 class DeveploeConfig(Config):
     DEBUG = True
-    
+
 
 class ReleaseConfig(Config):
-    DEBUG=False
+    DEBUG = False
+
 
 config = {
-    'develop':DeveploeConfig,
-    'release':ReleaseConfig
+    'develop': DeveploeConfig,
+    'release': ReleaseConfig
 }
+
